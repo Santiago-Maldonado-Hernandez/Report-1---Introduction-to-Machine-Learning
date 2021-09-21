4b5c1251e47eb00e9c07fbf4f1cd365703f4ce0d
Santiago-Maldonado-Hernandez <68322950+Santiago-Maldonado-Hernandez@users.noreply.github.com>
2021-09-21 15:26:09 +0200

    Range and table

--- 02450Toolbox_Matlab/project/project_1.m
+++ 02450Toolbox_Matlab/project/project_1.m
@@ -10,17 +10,28 @@ fclose(FID);
 file_path = fullfile(cdir,'../project/Data_abalone_dot.csv');
 % Table is now stored in Matlab
 abalone_table = readtable(file_path);
-X = table2array(abalone_table(:, 2:8));
+X = table2array(abalone_table(:, 2:9));
 
 % summary statistics for numerical attributes
 S = {'Length','Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight','Rings'};
+% summary stadistics
+% the median will be studied by the percentiles/quantile. The mean is the
+% quantile .5p. 
+stats_names = {'Mean' 'Quantiles' 'Variance' 'Standar deviation' 'range'};
+
 for i = 1:8
-    S(i)
-    mean(X(1:end,i))
-    quantile(X(1:end,i),[0 0.25 0.5 0.75 1])
-    var(X(1:end,i))
-    std(X(1:end,i))
+    S(i);
+    m(1, i) = mean(X(1:end,i));
+    q(i, :) = quantile(X(1:end,i),[0 0.25 0.5 0.75 1]);
+    v(1, i) = var(X(1:end,i));
+    s(1, i) = std(X(1:end,i));
+    r(1, i) = range(X(1:end, i));
 end
+
+% Create a table with the stadistics summary
+Stats = table(m', q, v', s', r', 'VariableNames', stats_names, 'RowNames', S');
+
+% study the covariance, and correlation
 
 % What to do with the nominal?
 mode(categorical(table2cell(abalone_table(:,1))))
