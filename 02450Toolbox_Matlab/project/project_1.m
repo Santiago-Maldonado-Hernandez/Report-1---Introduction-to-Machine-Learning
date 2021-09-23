369b31a72eadd17482ff2f0ab1b09b987b55cf47
Santiago-Maldonado-Hernandez <68322950+Santiago-Maldonado-Hernandez@users.noreply.github.com>
2021-09-23 15:23:11 +0200

    Update project_1.m

--- 02450Toolbox_Matlab/project/project_1.m
+++ 02450Toolbox_Matlab/project/project_1.m
@@ -1,4 +1,7 @@
 %% Project assignment 1
+clear all
+clc
+%% Initial Commands
 % The following code reads in the abalone dataset.
 cdir = fileparts(mfilename('fullpath'));
 % Since comma is used in the data, we need to convert these to dots
@@ -13,7 +16,7 @@ abalone_table = readtable(file_path);
 X = table2array(abalone_table(:, 2:9));
 
 % summary statistics for numerical attributes
-S = {'Length','Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight','Rings'};
+Sr = {'Length','Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight','Rings'};
 % summary stadistics
 % the median will be studied by the percentiles/quantile. The median is the
 % quantile .5p. 
@@ -35,7 +38,7 @@ for i = 1:8
 end
 
 % Create a table with the stadistics summary
-Stats = table(m', q, v', s', r', 'VariableNames', stats_names, 'RowNames', S');
+Stats = table(m', q, v', s', r', 'VariableNames', stats_names, 'RowNames', Sr');
 
 % study the covariance and correlation between atributes
 cov_matrix = cell(8, 8);
@@ -50,6 +53,74 @@ for i = 1:8
     end
 end
 
+
+%% Visualization
+
+% Subtract the mean from the data
+Y = bsxfun(@minus, X, mean(X));
+
+% Obtain the PCA solution by calculate the SVD of Y
+[U, S, V] = svd(Y);
+
+% Compute variance explained
+rho = diag(S).^2./sum(diag(S).^2);
+threshold = 0.95;
+
+% Plot variance explained
+mfig('NanoNose: Var. explained'); clf;
+hold on
+plot(rho, 'x-');
+plot(cumsum(rho), 'o-');
+plot([0,length(rho)], [threshold, threshold], 'k--');
+legend({'Individual','Cumulative','Threshold'}, ...
+        'Location','best');
+ylim([0, 1]);
+xlim([1, length(rho)]);
+grid minor
+xlabel('Principal component');
+ylabel('Variance explained value');
+title('Variance explained by principal components');
+
+% boxplots of the dimensions and different weights
+figure(2)
+boxplot(X(1:end,1:3),{'Length','Diameter','Height'})
+xlabel('Attribute')
+ylabel('Size (mm)')
+figure(3)
+boxplot(X(1:end,4:7),{'Whole weight','Shucked weight','Viscera weight','Shell weight'})
+xlabel('Attribute')
+ylabel('Weight (g)')
+
+% Histogram plots
+for i=1:8
+   figure(i+3)
+   H(i) = histogram(X(:, i));
+   xlabel(Sr(i));
+   title(['Histogram of the attribute ' Sr(i)])
+end
+
+% Plots distinguising the sex (M) or (F) or (I)
+sex = table2array(abalone_table(:, 1));
+figure(12)
+for i=1:4177
+   if(strcmp(sex{i, 1}, 'M'))
+       plot(X(i, 1), X(i,4), 'o', 'MarkerEdgeColor', 'b', 'MarkerFaceColor', 'b')
+       hold on
+   elseif (strcmp(sex{i, 1}, 'F'))
+       plot(X(i, 1), X(i,4), 'o', 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r')
+       hold on
+   else 
+       plot(X(i, 1), X(i,4), 'o', 'MarkerEdgeColor', 'y', 'MarkerFaceColor', 'y')
+       hold on
+   end
+end
+legend('Male', 'Female', 'Infant')
+xlabel('Length (mm)')
+ylabel('Whole weight')
+title('Length vs Whole wieght taking into account the sex')
+hold off
+
+%% 
 % What to do with the nominal?
 mode(categorical(table2cell(abalone_table(:,1))))
 % Sample text
